abf5c3ad98c8b7c5c4fea5b9ed66b8dcec15f1fd
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-01-06 20:57:18 +0600

    handling multiple props dilling

--- src/components/Cousin/Cousin.jsx
+++ src/components/Cousin/Cousin.jsx
@@ -1,10 +1,14 @@
+import Special from "../Special/Special";
 
 
-const Cousin = ({name}) => {
+const Cousin = ({name,asset}) => {
     return (
         <div>
             <h2>cousin</h2>  
-            <p>{name}</p>          
+            <p>{name}</p>    
+            <section>
+              {asset && <Special asset={asset}></Special>}
+                </section>      
         </div>
     );
 };
--- src/components/GrandPa/Granpa.jsx
+++ src/components/GrandPa/Granpa.jsx
@@ -1,19 +1,32 @@
+import { createContext } from "react";
 import Aunt from "../Aunt/Aunt";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
 
+// creating context api
+const AssetContext = createContext('gold')
+
 const Granpa = () => {
+    const asset ="diamond"
     return (
         <div className="grandpa">
             <h2>grandpa</h2>
-          <section className="flex">
-           <Dad></Dad>
-            <Uncle></Uncle>
+         <AssetContext.Provider value="gold">
+         <section className="flex">
+           <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
             <Aunt></Aunt>
           </section>
+         </AssetContext.Provider>
         </div>
     );
 };
 
 export default Granpa;
+
+
+
+// for using context api
+// 1.create a context and export it
+//2.add provider for the context with a value
