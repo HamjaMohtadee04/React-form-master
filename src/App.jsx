8d619455e3499de70b4ee232990e7863ad5581f9
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-01-06 20:07:25 +0600

    childer and props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 
 import './App.css'
+import ReusuableForm from './components/ReusableForm/ReusuableForm'
+// import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
-import StatefulForm from './components/StatefulForm/StatefulForm'
+// import StatefulForm from './components/StatefulForm/StatefulForm'
 
 function App() {
+
+  const handleSignupSubmit = (data) => {
+  console.log("sign up data",data)
+}
+  const handleUpdateProfile = (data) => {
+    console.log("update profile data",data)
+}
 
   return (
     <>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+    {/* <RefForm></RefForm> */}
+    <ReusuableForm 
+    formTitle={'Sign Up'}
+    handleSubmit={handleSignupSubmit}>
+   <div>
+    <h2>sign up</h2>
+    <p>sign up here</p>
+   </div>
+    </ReusuableForm>
+    <ReusuableForm formTitle={'Profile Update'} handleSubmit={handleUpdateProfile} submitBtnText={'Update'}>
+      <div>
+        <h2>update profile</h2>
+        <p>always keep your profile updated</p>
+      </div>
+    </ReusuableForm>
     </>
+
   )
 }
 
